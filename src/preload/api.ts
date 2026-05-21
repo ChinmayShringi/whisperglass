02d54802b2f1f4811d95d495d5d3aaed011e2714
ChinmayShringi <chinmayshringi4@gmail.com>
2026-05-20 23:41:05 -0400

    feat: expose Codex API in the preload bridge

--- src/preload/api.ts
+++ src/preload/api.ts
@@ -1,4 +1,12 @@
-import { IpcChannel, type OverlayState } from '../shared/types'
+import {
+  IpcChannel,
+  type OverlayState,
+  type AskQuestionRequest,
+  type AnswerChunk,
+  type AnswerResult,
+  type AnswerError,
+  type CodexStatus,
+} from '../shared/types'
 
 export interface IpcRendererLike {
   send(channel: string, ...args: unknown[]): void
@@ -9,6 +17,11 @@ export interface IpcRendererLike {
 export interface OverlayApi {
   toggleInvisibility(): void
   onOverlayState(callback: (state: OverlayState) => void): () => void
+  askQuestion(request: AskQuestionRequest): void
+  onAnswerChunk(callback: (chunk: AnswerChunk) => void): () => void
+  onAnswerDone(callback: (result: AnswerResult) => void): () => void
+  onAnswerError(callback: (error: AnswerError) => void): () => void
+  onCodexStatus(callback: (status: CodexStatus) => void): () => void
 }
 
 export function createOverlayApi(ipcRenderer: IpcRendererLike): OverlayApi {
@@ -20,5 +33,10 @@ export function createOverlayApi(ipcRenderer: IpcRendererLike): OverlayApi {
   return {
     toggleInvisibility: () => ipcRenderer.send(IpcChannel.ToggleInvisibility),
     onOverlayState: (callback) => subscribe(IpcChannel.OverlayState, callback),
+    askQuestion: (request) => ipcRenderer.send(IpcChannel.AskQuestion, request),
+    onAnswerChunk: (callback) => subscribe(IpcChannel.AnswerChunk, callback),
+    onAnswerDone: (callback) => subscribe(IpcChannel.AnswerDone, callback),
+    onAnswerError: (callback) => subscribe(IpcChannel.AnswerError, callback),
+    onCodexStatus: (callback) => subscribe(IpcChannel.CodexStatus, callback),
   }
 }
